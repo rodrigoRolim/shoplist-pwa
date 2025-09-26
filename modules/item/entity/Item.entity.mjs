f628817a65695776dda55bb77fcc55aeff03d8fc
Rodrigo Rolim <rodrigorolimveras92@gmail.com>
2025-09-26 00:27:09 -0300

    create item entity

---
+++ modules/item/entity/Item.entity.mjs
@@ -0,0 +1,8 @@
+export default class ItemEntity {
+  constructor(name, quantity, unit, unitPrice) {
+    this.name = name;
+    this.quantity = quantity;
+    this.unit = unit;
+    this.unitPrice = unitPrice;
+  }
+}
